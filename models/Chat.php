b91474bffb971ba000c80a936d3e73175f6d2611
Sergey Taranov <sergey73@yandex.ru>
2021-11-17 00:15:07 +0300

    Help text change

--- models/Chat.php
+++ models/Chat.php
@@ -4,7 +4,22 @@ namespace app\models;
 
 class Chat
 {
-    public const TEXT_HELP          = 'This is robot help';
+    public const TEXT_HELP          = <<<TEXT
+\xF0\x9F\x92\xB9	*HELP WITH CURRENCY BOT*
+    
+/list`        `- Get all available rates
+
+/lst`        ` - Get all available rates
+
+`/exchange ` - Converts with two decimal precision.
+`          `   Format: `$10 to CAD`; `10 USD to CAD`
+
+`/history  ` - Get history for period.
+`          `   Format: `USD/CAD for 7 days`
+
+\xF0\x9F\x92\xA1	Show help: /help
+TEXT;
+
     public const PARSE_MODE         = 'Markdown';
     public const TEXT_MAX_LENGTH    = 750;
 }
